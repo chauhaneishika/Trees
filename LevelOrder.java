fa11f6b08f2d35c6e03e996b0759e23fd331e1f7
eishikachauhan <chauhaneishika@gmail.com>
2026-09-04 09:50:34 +0530

    Level wise order traversal

---
+++ LevelOrder.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+class Pair {
+    Node node;
+    int level;
+    Pair(Node node, int level) {
+        this.node = node;
+        this.level = level;
+    }
+}
+
+public class LevelOrder {
+    public static void main(String[] args) {
+        Node a = new Node(3); //root
+        Node b = new Node(4);
+        Node c = new Node(2);
+        Node d = new Node(-1);
+        Node e = new Node(1);
+        Node f = new Node(6);
+        Node g = new Node(9);
+        a.left=b; a.right=c;
+        b.left=d; b.right=e;
+        c.left=f; c.right=g;
+        //levelOrder(a);
+        levelOrderLineWise(a);
+    }
+    // private static void levelOrder(Node root) {
+    //     Queue<Node> q = new LinkedList<>();
+    //     q.add(root);
+    //     while(!q.isEmpty()) {
+    //         Node front = q.remove();
+    //         System.out.print(front.val+" ");
+    //         if(front.left!=null) q.add(front.left);
+    //         if(front.right!=null) q.add(front.right);
+    //     }
+    //     System.out.println();
+    // }
+    private static void levelOrderLineWise(Node root) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root, 0));
+        int currentLevel = 0;
+        while(!q.isEmpty()) {
+            Pair front = q.remove();
+            Node node = front.node;
+            int level = front.level;
+            if(front.level != currentLevel) {
+                currentLevel++;
+                System.out.println();
+            }
+            System.out.print(front.node.val+" ");
+            if(front.node.left!=null) q.add(new Pair(front.node.left, front.level+1));
+            if(front.node.right!=null) q.add(new Pair(front.node.right, front.level+1));
+        }
+        System.out.println();
+    }
+}
