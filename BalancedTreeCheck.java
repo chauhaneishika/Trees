0bb92a115898710ab7bf795bda730603abf507e8
eishikachauhan <chauhaneishika@gmail.com>
2026-09-07 20:37:14 +0530

    Balanced Binary Tree

---
+++ BalancedTreeCheck.java
@@ -0,0 +1,27 @@
+public class BalancedTreeCheck {
+    // public boolean isBalanced(Node root) {
+    //     if(root == null) return true;
+    //     int leftLevels = levels(root.left);
+    //     int rightLevels = levels(root.right);
+    //     if(Math.abs(leftLevels-rightLevels) > 1) return false;
+    //     return isBalanced(root.left) && isBalanced(root.right);
+    // }
+    // private int levels(Node root) {
+    //     if(root == null) return 0;
+    //     return 1 + Math.max(levels(root.left),levels(root.right));
+    // }
+    static boolean flag;
+    public boolean isBalanced(Node root) {
+        if(root == null) return true;
+        flag = true;
+        levels(root);
+        return flag;
+    }
+    private int levels(Node root){
+        if(root == null) return 0;
+        int leftLevels = levels(root.left);
+        int rightLevels = levels(root.right);
+        if(Math.abs(leftLevels-rightLevels) > 1) flag = false;
+        return 1 + Math.max(leftLevels,rightLevels);
+    }
+}
