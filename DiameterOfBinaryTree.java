4d2e8ac46a6cccfbba754339316e072628a0be01
eishikachauhan <chauhaneishika@gmail.com>
2026-09-08 20:24:25 +0530

    Calculate Max Diameter of Binary Tree

---
+++ DiameterOfBinaryTree.java
@@ -0,0 +1,15 @@
+public class DiameterOfBinaryTree {
+    static int max;
+    int diameter(Node root) {
+        max = 0;
+        levels(root);
+        return max;
+    }
+    private int levels(Node root){
+        if(root == null) return 0;
+        int leftLevels = levels(root.left);
+        int rightLevels = levels(root.right);
+        max = Math.max(max, leftLevels + rightLevels);
+        return 1 + Math.max(leftLevels, rightLevels);
+    }
+}
